a77c2b8ead1492f620a8119027785710b327ab28
logan6230 <pantoja2187@gmail.com>
2022-08-21 15:00:37 -0500

    Crear funciones y asociar al front

--- script.js
+++ script.js
@@ -1,65 +1,86 @@
 console.log("pase por aqui");
 var listaVocales = ["e", "o", "i", "a", "u"];
 var listaCriptos = ["enter", "ober", "imes", "ai", "ufat"];
-textArea();
-function textArea() {
-    document.getElementById("right").innerHTML = '<div class="img-muñeco"><img src="img/Muñeco.png" alt="muñeco con lupa"></div><div id="foto"></div><div><h1>Ningun mensaje fue encontrado</h1></div><form action=""><textarea name="" id="" cols="30" rows="10"placeholder="Ingrese el texto que dese encriptar o desencriptar"></textarea></form><div d="desencriptado"></div>'
+var validador = document.getElementById('box-msj')
+    /*textArea();
 
-}
+    function textArea() {
+        document.getElementById("right").innerHTML = '<div class="img-muñeco"><img src="img/Muñeco.png" alt="muñeco con lupa"></div><div id="foto"></div><div><h1>Ningun mensaje fue encontrado</h1></div><form action=""><textarea name="" id="" cols="30" rows="10"placeholder="Ingrese el texto que dese encriptar o desencriptar"></textarea></form><div d="desencriptado"></div>'
+
+    }*/
 
 function encriptar() {
-    var texto = document.getElementById("taMensaje");
-    var texto2 = texto.value
-    var validacion = validarMayusculas(texto2)
-    var contador = 0
-    if (validacion) {
-
-        for (let i = 0; i < listaVocales.length; i++) {
-            if (contador <= 0) {
-                console.log('En el if : ' + listaVocales[i])
-                let vocal = new RegExp(listaVocales[i], 'img')
-                var encriptar2 = texto2.replace(vocal, listaCriptos[i]);
-                contador += 1
-            } else {
-                let vocal = new RegExp(listaVocales[i], 'img')
-                console.log('Expresion 2 : ' + vocal)
-                var encriptar2 = encriptar2.replace(vocal, listaCriptos[i]);
-            }
-            document.getElementById("right").innerHTML = '<textarea >' + encriptar2 + '</textarea><button>Copiar</button>'
-        }
+    if (document.getElementById('box-msj').value == "" || document.getElementById('box-msj').value.length < 2) {
+        alert("Debe escribir o pegar un un texto ue tenga al menos dos letras")
     } else {
+        var texto = document.getElementById("box-msj");
+        var texto2 = texto.value
+        console.log("pase por aqui" + texto2);
+        var validacion = validarMayusculas(texto2)
+        var contador = 0
+        if (validacion) {
 
-        alert("El texto ingresado no debe contener: mayusculas\nacentos \nEl unico caracteres especial admitido es ! ")
+            for (let i = 0; i < listaVocales.length; i++) {
+                if (contador <= 0) {
+                    console.log('En el if : ' + listaVocales[i])
+                    let vocal = new RegExp(listaVocales[i], 'img')
+                    var encriptar2 = texto2.replace(vocal, listaCriptos[i]);
+                    contador += 1
+                } else {
+                    let vocal = new RegExp(listaVocales[i], 'img')
+                    console.log('Expresion 2 : ' + vocal)
+                    var encriptar2 = encriptar2.replace(vocal, listaCriptos[i]);
+                }
+                console.log(encriptar2)
+
+            }
+        } else {
+
+            alert("El texto ingresado no debe contener:\nMenos de una letra\nmayusculas\nacentos\nEl unico caracteres especial admitido es ! ")
+        }
+        if (validacion) {
+            document.getElementById("show-msj").innerHTML = '<textarea id="msjCopiar">' + encriptar2 + '</textarea><button class="btn-copiar" onclick="copiarTexto()">Copiar</button>'
+            document.getElementById("containerBoxMsj").innerHTML = '<textarea id="box-msj" rows="37" class="container-text" placeholder="Escriba Escriba o pegue su texto aqui..."></textarea> ';
+        }
     }
 }
 
 function desencriptar() {
-    var texto = document.getElementById("taMensaje");
-    var texto2 = texto.value
-    var validacion = validarMayusculas(texto2)
-    var contador = 0
-    if (validacion) {
-        for (let i = 0; i < listaVocales.length; i++) {
-            console.log('primer for: ' + texto2[i])
-            if (contador <= 0) {
-                console.log('En el if : ' + listaVocales[i])
-                let cripto = new RegExp(listaCriptos[i], 'img')
-                var encriptar2 = texto2.replace(cripto, listaVocales[i]);
-                contador += 1
-            } else {
-                let cripto = new RegExp(listaCriptos[i], 'img')
-                console.log('Expresion 2 : ' + cripto)
-                var encriptar2 = encriptar2.replace(cripto, listaVocales[i]);
+    if (document.getElementById('box-msj').value == "" || document.getElementById('box-msj').value.length < 2) {
+        alert("Debe escribir o pegar un un texto ue tenga al menos dos letras")
+    } else {
+        console.log('validador' + document.getElementById('box-msj').value)
+        var texto = document.getElementById("box-msj");
+        var texto2 = texto.value
+        var validacion = validarMayusculas(texto2)
+        var contador = 0
+        if (validacion) {
+            for (let i = 0; i < listaVocales.length; i++) {
+                console.log('primer for: ' + texto2[i])
+                if (contador <= 0) {
+                    console.log('En el if : ' + listaVocales[i])
+                    let cripto = new RegExp(listaCriptos[i], 'img')
+                    var encriptar2 = texto2.replace(cripto, listaVocales[i]);
+                    contador += 1
+                } else {
+                    let cripto = new RegExp(listaCriptos[i], 'img')
+                    console.log('Expresion 2 : ' + cripto)
+                    var encriptar2 = encriptar2.replace(cripto, listaVocales[i]);
+                }
+                console.log('prueba de repeticion' + encriptar2)
             }
-            document.getElementById("right").innerHTML = '<textarea>' + encriptar2 + '</textarea>'
+
+        } else {
+            alert("El texto ingresado no debe contener: mayusculas\nacentos \nEl unico caracteres especial admitido es ! ")
+        }
+        if (validacion) {
+            document.getElementById("show-msj").innerHTML = '<textarea id="msjCopiar">' + encriptar2 + '</textarea><button class="btn-copiar" onclick="copiarTexto()">Copiar</button>'
+            document.getElementById("containerBoxMsj").innerHTML = '<textarea id="box-msj" rows="37" class="container-text" placeholder="Escriba o pegue su texto aqui..."></textarea> ';
         }
 
-    } else {
-
-        alert("El texto ingresado no debe contener: mayusculas\nacentos \nEl unico caracteres especial admitido es ! ")
     }
-
 }
+
 
 function validarMayusculas(texto) {
     var expresion = new RegExp("^[^-][a-z-!\\s]+$");
@@ -70,4 +91,9 @@ function validarMayusculas(texto) {
         return false;
     }
 
+}
+
+function copiarTexto() {
+    document.getElementById("msjCopiar").select()
+    document.execCommand('copy');
 }
